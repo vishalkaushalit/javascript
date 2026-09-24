265516053b9452d45a07155648e9efb4663288ef
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-09-24 12:22:02 +0000

    Variable Conversion file added

---
+++ 4_variablesConversion.js
@@ -0,0 +1,6 @@
+let score = 32;
+console.log(typeof age); // number
+console.log(typeof userName); // string
+console.log(typeof undefiend); // undefined
+console.log(typeof null); // object
+console.log(typeof Object); // function
