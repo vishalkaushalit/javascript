8f8fa47eece39b966fe964118409ca3d1527cd5d
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-09-24 12:10:54 +0000

    Datatypes file added

---
+++ 3_datatypes.js
@@ -0,0 +1,24 @@
+let userName = "Demo";
+let age = 32;
+let isLoggedIn = false;
+let state;
+/* 
+
+Primitive datatypes 
+    number => 2 to power 53
+    bigint 
+    string  => ""
+    boolean  => true/false
+    null  => standalone value
+    undefined  => 
+    symbol => unique
+
+Non-Primitive datatypes 
+    Object => function
+*/
+
+console.log(typeof age); // number
+console.log(typeof userName); // string
+console.log(typeof undefiend); // undefined
+console.log(typeof null); // object
+console.log(typeof Object); // function
